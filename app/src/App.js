3614ad050c29ac58cf1847ee2cf77853009f4ba0
Kelvin <kelvinyelyen@gmail.com>
2023-03-13 16:27:33 +0000

    App overhaul

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Widgets from './components/Widgets';
-
-import DataContextProvider from './contexts/DataContext';
-
-
 
 function App() {
   return (
     <div className="App">
-      <DataContextProvider>
-        <Widgets/>
-      </DataContextProvider>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
